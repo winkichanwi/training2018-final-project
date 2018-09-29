17d16752b61fa0c961e5f277ac1e4857bfe28625
Winki Chan <chanwingin02@gmail.com>
2018-09-29 19:03:39 +0900

    Edit test of AuthController

--- restaurant-remote-reception/test/controllers/AuthControllerSpec.scala
+++ restaurant-remote-reception/test/controllers/AuthControllerSpec.scala
@@ -1,23 +1,20 @@
 package controllers
 
 import models.Constants
-import org.scalatest.mockito.MockitoSugar
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.Mode
-
 import play.api.db.{Database, Databases}
+
 import play.api.test._
 import play.api.test.Helpers._
 import play.api.mvc.Result
 import play.api.db.slick.DatabaseConfigProvider
-
 import play.api.inject.Injector
+
 import play.api.inject.guice.GuiceApplicationBuilder
 import scala.concurrent.Future
 import play.api.inject.bind
-
-import scala.concurrent.ExecutionContext.Implicits.global
 
 class AuthControllerSpec extends PlaySpec with GuiceOneAppPerTest {
 
@@ -30,15 +27,16 @@ class AuthControllerSpec extends PlaySpec with GuiceOneAppPerTest {
         )
     )
 
-    implicit lazy val appBuilder: GuiceApplicationBuilder = new GuiceApplicationBuilder()
+    lazy val appBuilder: GuiceApplicationBuilder = new GuiceApplicationBuilder()
         .in(Mode.Test)
         .overrides(bind[Database].toInstance(database))
     lazy val injector: Injector = appBuilder.injector()
     lazy val dbConfProvider: DatabaseConfigProvider = injector.instanceOf[DatabaseConfigProvider]
+    implicit lazy val application = appBuilder.build()
 
     "authenticate" should {
         "should be valid if session user exists" in {
-            val controller = new AuthController(dbConfProvider)
+            val controller = application.injector.instanceOf(classOf[AuthController])
 
             val defaultUserId = "1"
             val request = FakeRequest().withSession(Constants.SESSION_TOKEN_USER_ID -> defaultUserId)
@@ -47,7 +45,7 @@ class AuthControllerSpec extends PlaySpec with GuiceOneAppPerTest {
         }
 
         "should be invalid if session user does not exists" in {
-            val controller = new AuthController(dbConfProvider)
+            val controller = application.injector.instanceOf(classOf[AuthController])
 
             val fakeUserId = "0"
             val request = FakeRequest().withSession(Constants.SESSION_TOKEN_USER_ID -> fakeUserId)
